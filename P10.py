d3f033901e31be344c5e93fb1cd4ad5142e366ce
Angelopezmacc <49574428+Angelopezmacc@users.noreply.github.com>
2022-03-24 21:45:31 -0500

    Update P10.py

--- P10.py
+++ P10.py
@@ -1,23 +1,26 @@
 # Import of RegEx library
 import re
 
+# Function that prevents XSS and SQLi
 def no_XSS_or_SQLi (text):
-    notAllowed = ["<script>"," ' ","</script>","<",">" ]
+    
+    notAllowed = ["<script>","'","</script>","<",">",] #List of banned words
     coincidences = 0
+    
     for i in range(len(notAllowed)):
+        
         validate = re.findall(notAllowed[i], text)
         if len(validate) >= 1:
             coincidences += 1
         
     if coincidences >= 1:
         print("lA ENTRADA NO ES VÁLIDA. VUELVA A INTENARLO")
+        
     if coincidences == 0:
         print("Entrada correcta.")
 
 no_XSS_or_SQLi("<script> alert('document.cookie') </script>")
-no_XSS_or_SQLi("<")
-
-
+no_XSS_or_SQLi("'")
 
 
 
