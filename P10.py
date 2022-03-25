a487121c4105f099515b639c135d78460ab93d28
Angelopezmacc <49574428+Angelopezmacc@users.noreply.github.com>
2022-03-24 21:20:56 -0500

    Add files via upload

---
+++ P10.py
@@ -0,0 +1,23 @@
+# Import of RegEx library
+import re
+
+def no_XSS_or_SQLi (text):
+    notAllowed = ["<script>"," ' ","</script>","<",">" ]
+    coincidences = 0
+    for i in range(len(notAllowed)):
+        validate = re.findall(notAllowed[i], text)
+        if len(validate) >= 1:
+            coincidences += 1
+        
+    if coincidences >= 1:
+        print("lA ENTRADA NO ES VÁLIDA. VUELVA A INTENARLO")
+    if coincidences == 0:
+        print("Entrada correcta.")
+
+no_XSS_or_SQLi("<script> alert('document.cookie') </script>")
+no_XSS_or_SQLi("<")
+
+
+
+
+
